1743ea9bd32e80c782f97753d91f006e2c97234b
Ricardo Cruz <ricardocruzm2301@gmail.com>
2026-05-12 18:50:27 -0300

    Add files via upload

---
+++ gerenciar_favoritos.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['favoritos'])) {
+    $_SESSION['favoritos'] = [];
+}
+
+$sku = $_GET['sku'] ?? null;
+$acao = $_GET['acao'] ?? 'add';
+
+if ($sku) {
+    if ($acao === 'add' && !in_array($sku, $_SESSION['favoritos'])) {
+        $_SESSION['favoritos'][] = $sku;
+    } elseif ($acao === 'remove') {
+        $posicao = array_search($sku, $_SESSION['favoritos']);
+        if ($posicao !== false) {
+            unset($_SESSION['favoritos'][$posicao]);
+        }
+    }
+}
+
+header("Location: index.php");
+exit;
